6e231d9d3b8c10a398fa3d906dbb60a1b35ca4af
Taofiano <jtaofi0129@naver.com>
2023-08-28 19:25:34 +0900

    허프변환 성공 각 line들 각도 arctan으로 구해서 최소, 최대 각도에 따른 line들 선별하여 표시,
    각 line들의 좌표를 활용해 평균 직선생성하여 중앙선 생성, 이후 유로트럭 결합하여 steering 제어

--- lane_detection.py
+++ lane_detection.py
@@ -2,6 +2,11 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+'''
+def onChange 및 'canny'라는 창의 주석들 : 
+Canny edge 검출 시 low_T, high_T 트랙바 활용해 실험.
+'''
+
 class Canny_edge_lane:
     def __init__(self, cap):
         self.cap = cap
@@ -24,16 +29,39 @@ class Canny_edge_lane:
         cv2.fillPoly(mask, vertices, 255)
         roi_frame = cv2.bitwise_and(frame, mask)
         return roi_frame
+    def hough(self, frame):
+        h, w = frame.shape
+        lines = cv2.HoughLinesP(frame, rho=1, theta=np.pi/180, threshold=50, minLineLength=10, maxLineGap=30)
+        line_img = np.zeros((h, w, 3), dtype=np.uint8)
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                # color : [B, G, R]
+                cv2.line(line_img, (x1, y1), (x2, y2), color=[255, 0, 0], thickness=2)
+        return line_img
 
+    def control_hough_lines(self, line):
+        return 0
+
+    # def onChange(self, x):
+    #     pass
     def open_vid(self):
         count = 0
+        # cv2.namedWindow('canny')
+        # cv2.createTrackbar('low_T', 'canny', 0, 255, self.onChange)
+        # cv2.createTrackbar('high_T', 'canny', 0, 255, self.onChange)
+
+        # cv2.namedWindow('canny')
+        # cv2.createTrackbar('low_T', 'canny', 0, 255, self.onChange)
+        # cv2.createTrackbar('high_T', 'canny', 0, 255, self.onChange)
         while True:
             # 동영상 파일 존재 여부(True/False)와 현재 프레임 이미지를 읽음
             retval, frame = self.cap.read()
             gray_frame = self.grayscale(frame)
-            gaussian_blur_frame = self.gaussian_blur(gray_frame, kernel_size=5)
-            canny_frame = self.canny(gaussian_blur_frame, low_t=50, high_t=200)
+            # 가우시안 블러 kernel_size:3 cause: Sobel kernel to be used internally
+            gaussian_blur_frame = self.gaussian_blur(gray_frame, kernel_size=3)
+            canny_frame = self.canny(gaussian_blur_frame, low_t=130, high_t=200)
             roi_frame = self.roi(canny_frame)
+            hough_frame = self.hough(roi_frame)
 
             # 만약 동영상 파일이 존재하지 않으면 while 반복문 종료
             if retval == False:
@@ -48,7 +76,7 @@ class Canny_edge_lane:
             # Tab 키로 color, gray scale 영상 변환 출력
             elif key == 9:
                 count += 1
-                if count == 5:
+                if count == 6:
                     count = 0
 
             if count == 0:
@@ -58,9 +86,14 @@ class Canny_edge_lane:
             elif count == 2:
                 cv2.imshow('road_driving', gaussian_blur_frame)
             elif count == 3:
+                # low_t = cv2.getTrackbarPos('low_T', 'canny')
+                # high_t = cv2.getTrackbarPos('high_T', 'canny')
+                # canny_frame = self.canny(gaussian_blur_frame, low_t, high_t)
                 cv2.imshow('road_driving', canny_frame)
             elif count == 4:
                 cv2.imshow('road_driving', roi_frame)
+            elif count == 5:
+                cv2.imshow('road_driving', hough_frame)
 
         # 동영상 파일 닫고 모든창 종료
         self.cap.release()
