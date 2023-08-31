bdb82419ad57dea25161909558bcf186dd57d914
Taofiano <jtaofi0129@naver.com>
2023-08-31 19:59:12 +0900

    23.08.31 (목) 현재 상황 - 원하는 좌우 차선 구분까지는 완료 but,   각 선들의 각도가 모두 130 ~ 180 사이임..

--- coding_basic/practice.py
+++ coding_basic/practice.py
@@ -7,6 +7,4 @@ lines = [[1, 2, 3, 4],
 
 lines = np.array(lines)
 
-deg = np.abs(np.rad2deg(np.arctan2(lines[:, 1] - lines[:, 3], lines[:, 0] - lines[:, 2])))
-print(deg[deg < 130])
-print(deg)
+print(lines[:, 0])
--- lane_detection.py
+++ lane_detection.py
@@ -42,7 +42,7 @@ class Canny_edge_lane:
             cv2.line(line_img, (x1, y1), (x2, y2), color=[255, 0, 0], thickness=2)
         return line_img
 
-    def lines_wanted(self, lines, min_deg, max_deg):
+    def lines_wanted(self, lines, min_deg=130, max_deg=180):
         lines = np.squeeze(lines)
         theta_deg = np.abs(np.rad2deg(np.arctan2(lines[:, 1] - lines[:, 3], lines[:, 0] - lines[:, 2])))
         lines = lines[np.abs(theta_deg) < max_deg]
@@ -54,21 +54,70 @@ class Canny_edge_lane:
         # np.append(lines_new, lines[np.abs(theta_deg) > 110])
         # np.append(lines_new, lines_new[np.abs(theta_deg) > 30])
         return lines, theta_deg
+    def special_lines(self, frame, line_classify_count=0):
+        h, w = frame.shape
+        line_img = np.zeros((h, w, 3), dtype=np.uint8)
+        lines = cv2.HoughLinesP(frame, rho=1, theta=np.pi / 180, threshold=50, minLineLength=10, maxLineGap=30)
+        lines, theta_deg = self.lines_wanted(lines, min_deg=130, max_deg=180)
 
-    def control_hough_lines(self, line):
-        return 0
+        line_arr_1 = np.empty((1, 4), dtype=float)
+        line_arr_2 = np.empty((1, 4), dtype=float)
 
-    def onChange(self, x):
-        pass
+        for index in range(theta_deg.size):
+
+            if index <= theta_deg.size - 1:
+                if theta_deg[index] - theta_deg[index + 1] < 7:
+                    if line_classify_count == 0:
+                        np.append(line_arr_1, lines[index], axis=0)
+                        print(line_arr_1)
+                    elif line_classify_count == 1:
+                        np.append(line_arr_2, lines[index], axis=0)
+
+                elif theta_deg[index] - theta_deg[index + 1] >= 7:
+                    line_classify_count += 1
+                    np.append(line_arr_1, theta_deg[index], axis=0)
+                    break
+        print(line_arr_1)
+        line1_x1 = line_arr_1(axis=0) / theta_deg.size
+        line1_y1 = line_arr_1(axis=1) / theta_deg.size
+        line1_x2 = line_arr_1(axis=2) / theta_deg.size
+        line1_y2 = line_arr_1(axis=3) / theta_deg.size
+
+        line2_x1 = line_arr_2(axis=0) / theta_deg.size
+        line2_y1 = line_arr_2(axis=1) / theta_deg.size
+        line2_x2 = line_arr_2(axis=2) / theta_deg.size
+        line2_y2 = line_arr_2(axis=3) / theta_deg.size
+
+        # line1_x1 = np.sum(line_list_1[:, 0]) / theta_deg.size
+        # line1_y1 = np.sum(line_list_1[:, 1]) / theta_deg.size
+        # line1_x2 = np.sum(line_list_1[:, 2]) / theta_deg.size
+        # line1_y2 = np.sum(line_list_1[:, 3]) / theta_deg.size
+        #
+        # line2_x1 = np.sum(line_list_2[:, 0]) / theta_deg.size
+        # line2_y1 = np.sum(line_list_2[:, 1]) / theta_deg.size
+        # line2_x2 = np.sum(line_list_2[:, 2]) / theta_deg.size
+        # line2_y2 = np.sum(line_list_2[:, 3]) / theta_deg.size
+
+        cv2.line(line_img, (line1_x1, line1_y1), (line1_x2, line1_y2), color=[0, 0, 255], thickness=3)
+        cv2.line(line_img, (line2_x1, line2_y1), (line2_x2, line2_y2), color=[0, 255, 0], thickness=3)
+
+        return line_img
+
+    # Trackbar 생성을 위한 pass 함수
+    # def onChange(self, x):
+    #     pass
     def open_vid(self):
         count = 0
+
+        # canny 라인을 위한 Threshold 실험값 측정 트랙바
         # cv2.namedWindow('canny')
         # cv2.createTrackbar('low_T', 'canny', 0, 255, self.onChange)
         # cv2.createTrackbar('high_T', 'canny', 0, 255, self.onChange)
 
-        cv2.namedWindow('deg_cont')
-        cv2.createTrackbar('min_deg', 'deg_cont', 0, 180, self.onChange)
-        cv2.createTrackbar('max_deg', 'deg_cont', 0, 180, self.onChange)
+        # hough 변환, 차선 detection 최소, 최대 각도 실험값 측정 트랙바
+        # cv2.namedWindow('deg_cont')
+        # cv2.createTrackbar('min_deg', 'deg_cont', 0, 180, self.onChange)
+        # cv2.createTrackbar('max_deg', 'deg_cont', 0, 180, self.onChange)
 
         while True:
             # 동영상 파일 존재 여부(True/False)와 현재 프레임 이미지를 읽음
@@ -93,7 +142,7 @@ class Canny_edge_lane:
             # Tab 키로 color, gray scale 영상 변환 출력
             elif key == 9:
                 count += 1
-                if count == 6:
+                if count == 7:
                     count = 0
 
             if count == 0:
@@ -110,11 +159,17 @@ class Canny_edge_lane:
             elif count == 4:
                 cv2.imshow('road_driving', roi_frame)
             elif count == 5:
-                min_deg = cv2.getTrackbarPos('min_deg', 'deg_cont')
-                max_deg = cv2.getTrackbarPos('max_deg', 'deg_cont')
-                hough_frame = self.hough(roi_frame, min_deg, max_deg)
+                # min_deg = cv2.getTrackbarPos('min_deg', 'deg_cont')
+                # max_deg = cv2.getTrackbarPos('max_deg', 'deg_cont')
+                hough_frame = self.hough(roi_frame, min_deg=130, max_deg=180)
 
-                cv2.imshow('deg_cont', hough_frame)
+                # cv2.imshow('deg_cont', hough_frame)
+                cv2.imshow('road_driving', hough_frame)
+            elif count == 6:
+                special_frame = self.special_lines(roi_frame)
+
+                # cv2.imshow('deg_cont', hough_frame)
+                cv2.imshow('road_driving', special_frame)
 
         # 동영상 파일 닫고 모든창 종료
         self.cap.release()
